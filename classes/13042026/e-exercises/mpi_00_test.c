547ccfca069adb4d1f4d176a886247e50247d2da
yeferson59 <116679774+yeferson59@users.noreply.github.com>
2026-04-13 11:32:47 -0500

    feat: add simple MPI test program printing rank/size

---
+++ classes/13042026/e-exercises/mpi_00_test.c
@@ -0,0 +1,18 @@
+#include <mpi.h>
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+  MPI_Init(&argc, &argv);
+
+  int world_rank, world_size;
+
+  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+  printf("Process ID (PID) %d of %d\n", world_rank, world_size);
+
+  MPI_Finalize();
+
+  return 0;
+}
